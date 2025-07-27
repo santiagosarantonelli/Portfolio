0f5367bb36fbb0002f0040aaed47a233deba0ecc
Santiago Nicolas Sarantonelli <ssarantonelli7@gmail.com>
2025-07-27 19:11:03 +0200

    update main

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/CSS Tarea entregable 4 - Portfolio/', // Usa rutas relativas en los enlaces generados en el HTML
+});
